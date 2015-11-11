ddd7aa5cc72253d56b4e3ce58dee19b769f4e85f
Schlomo Schapiro <schlomo@schapiro.org>
2015-11-11 22:43:54 +0100

    moved to jts-base

--- src/jts-guest-customization/src/etc/guest-session/auto.sh
+++ src/jts-guest-customization/src/etc/guest-session/auto.sh
@@ -5,9 +5,3 @@ gsettings set com.canonical.indicator.session suppress-restart-menuitem true
 gsettings set com.canonical.indicator.session suppress-shutdown-menuitem true
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Alt>Shift_L']"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'de'), ('xkb', 'il')]"
-#gsettings set com.canonical.indicator.datetime show-day true
-#gsettings set com.canonical.indicator.datetime show-seconds true
-#gsettings set com.canonical.indicator.datetime show-date true
-#gsettings set com.canonical.indicator.datetime show-week-numbers true
-#gsettings set com.canonical.indicator.datetime show-year true
-#gsettings set com.canonical.Unity.Launcher favorites "['application://google-chrome.desktop', 'application://nautilus.desktop', 'application://libreoffice-writer.desktop', 'application://libreoffice-calc.desktop', 'application://libreoffice-impress.desktop', 'unity://running-apps', 'application://gnome-terminal.desktop', 'unity://expo-icon', 'unity://devices']"

0d6662ec6b3e41b03b2f7a02ded33d24c96b193f
Schlomo Schapiro <schlomo@schapiro.org>
2015-12-20 14:37:26 +0100

    fix removal of old packages

--- src/putinrepo.sh
+++ src/putinrepo.sh
@@ -61,8 +61,10 @@ if [[ "${1:-}" ]] ; then
         else
             cp "$f" "$REPO_BASE_DIR"/deb/"$canonical_name" && echo "OK"
             all_packages=( $(ls -rt "$REPO_BASE_DIR"/deb/"$package_name"* ) )
+            declare -p all_packages
             if (( ${#all_packages[*]} > 2 )) ; then
-                unset 'all_packages[-2:2]'
+                unset 'all_packages[-1]'
+                unset 'all_packages[-1]'
                 if (( ${#all_packages[*]} > 0 )) ; then
                     echo "Removing old packages:"
                     rm -fv "${all_packages[@]}"

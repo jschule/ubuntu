a340babbed46120f2748d241df21fe2936ce63b1
Schlomo Schapiro <schlomo@schapiro.org>
2015-12-20 16:20:54 +0100

    Add background info for guest user

--- src/jts-guest-customization/src/etc/guest-session/auto.sh
+++ src/jts-guest-customization/src/etc/guest-session/auto.sh
@@ -5,3 +5,5 @@ gsettings set com.canonical.indicator.session suppress-restart-menuitem true
 gsettings set com.canonical.indicator.session suppress-shutdown-menuitem true
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Alt>Shift_L']"
 gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'de'), ('xkb', 'il')]"
+gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/jts-guestuser.svg'
+gsettings set org.gnome.desktop.background picture-options 'scaled'

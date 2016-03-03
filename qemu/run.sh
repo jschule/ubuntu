e2267c8efcfad272b1f516ab23affc0b5d2a6cf8
Schlomo Schapiro <schlomo@schapiro.org>
2016-03-03 23:06:09 +0100

    Experimentally exchange jts and google repo to track down xenial installation problems.

--- qemu/run.sh
+++ qemu/run.sh
@@ -4,6 +4,7 @@
 
 set -x
 
-ubuntu=${1:-wily}
-qemu-img create $ubuntu.raw 5G
-exec qemu-system-x86_64 -name JTS-$ubuntu -vga std -hda $ubuntu.raw -m 1024 -kernel ipxe.lkrn -append "dhcp && chain http://jschule.github.io/ubuntu/qemu/$ubuntu.txt" "$@"
+ubuntu=${1:-wily} ; shift
+
+qemu-img create $ubuntu.raw 10G
+exec qemu-system-x86_64 -name JTS-$ubuntu -vga std -drive file=$ubuntu.raw,if=virtio,cache=unsafe -m 1024 -kernel ipxe.lkrn -append "dhcp && chain http://jschule.github.io/ubuntu/qemu/$ubuntu.txt" "$@"

60232058ce963ff69a65446697bef6d499d0e95d
Schlomo Schapiro <schlomo@schapiro.org>
2016-03-03 18:20:08 +0100

    Pass extra options to qemu.

--- qemu/run.sh
+++ qemu/run.sh
@@ -6,4 +6,4 @@ set -x
 
 ubuntu=${1:-wily}
 qemu-img create $ubuntu.raw 5G
-exec qemu-system-x86_64 -name JTS-$ubuntu -vga std -hda $ubuntu.raw -m 1024 -kernel ipxe.lkrn -append "dhcp && chain http://jschule.github.io/ubuntu/qemu/$ubuntu.txt"
+exec qemu-system-x86_64 -name JTS-$ubuntu -vga std -hda $ubuntu.raw -m 1024 -kernel ipxe.lkrn -append "dhcp && chain http://jschule.github.io/ubuntu/qemu/$ubuntu.txt" "$@"

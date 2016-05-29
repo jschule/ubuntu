fc2ba59f3900a90ac4322be3f4d98f41417db1ee
Schlomo Schapiro <schlomo@schapiro.org>
2016-05-29 13:32:00 +0200

    Add new key for Schlomo

--- src/jts-repo/update-keyring.sh
+++ src/jts-repo/update-keyring.sh
@@ -1,1 +1,1 @@
-gpg --armor --export 9E62229E | tee ../../repo.key | gpg --no-default-keyring --primary-keyring ./src/etc/apt/trusted.gpg.d/jschule.gpg --import
+gpg --armor --export 9E62229E AD649AF2 | tee ../../repo.key | gpg --no-default-keyring --primary-keyring ./src/etc/apt/trusted.gpg.d/jschule.gpg --import
